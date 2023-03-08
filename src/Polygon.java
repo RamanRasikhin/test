d8bd449fb591e9aa461094d7703ab2f647c81363
student <romanrosikhin@gmail.com>
2023-03-08 15:57:51 +0100

    test

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,17 +1,20 @@
 import java.util.Locale;
 
-public class Polygon {
+public class Polygon extends Shape {
     private Point[] arr;
     private Style styleSettings;
-    public Polygon(int count, Style styleSettings)
+    public Polygon(int count, Style styleSettings, Style style)
     {
+        super(style);
         arr = new Point[count];
         this.styleSettings = styleSettings;
     }
-    public Polygon(int count) {
+    public Polygon(int count, Style styleSettings)
+    {
+        super(new Style(null, null, 0));
         arr = new Point[count];
+        this.styleSettings = styleSettings;
     }
-
     public void setPoint(int index, Point point) {
         arr[index] = point;
     }
