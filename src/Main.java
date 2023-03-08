3aa1f3e0ec28a93fb63086848f7e24e2a468c04e
student <romanrosikhin@gmail.com>
2023-03-08 15:32:43 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 public class Main {
-
     public static final Point test = new Point(2,3);
     public static void main(String[] args) {
         Point point= new Point(2,3);
         point=null;
         Double test=2.3;
         test=null;
+        System.out.println(point.test);
+        Point point2= new Point(1, 2);
+        point2.test=3;
+        System.out.println(point2.test);
     }
 
 
--- src/Point.java
+++ src/Point.java
@@ -1,5 +1,6 @@
 public class Point {
      public double x,y;
+     public static int test=2;
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
